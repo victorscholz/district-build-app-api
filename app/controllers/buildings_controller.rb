8f17ee10639b7d1b7e0cdc4a35fffd50fbb0cd56
victorscholz <vgscholz@gmail.com>
2020-10-02 01:30:02 -0400

    JWT Auth set upup

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,7 @@
 class BuildingsController < ApplicationController
+
+	skip_before_action :authorized
+
 	def index
 		buildings = Building.all
 		render json: buildings
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
-	# skip_before_action :authorized, only: [:create]
+	skip_before_action :authorized, only: [:create]
 
 	# def profile
 	# 	render json: { user: UserSerializer.new(current_user) },
 	# 	       status: :accepted
 	# end
 
-	# def create
-	# 	user = User.create(user_params)
-	# 	if user.valid?
-	# 		token = encode_token({ user_id: user.id })
-	# 		render json: { user: UserSerializer.new(user), jwt: token },
-	# 		       status: :created
-	# 	else
-	# 		render json: { error: 'failed to create user' },
-	# 		       status: :not_acceptable
-	# 	end
-	# end
+	def create
+		user = User.create(user_params)
+		if user.valid?
+			token = encode_token({ user_id: user.id })
+			render json: { user: UserSerializer.new(user), jwt: token },
+			       status: :created
+		else
+			render json: { error: 'failed to create user' },
+			       status: :not_acceptable
+		end
+	end 
 
 	def index
 		users = User.all
@@ -30,10 +30,10 @@ class UsersController < ApplicationController
 		       }
 	end
 
-	def create
-		user = User.create(user_params)
-		render json: user
-	end
+	# def create
+	# 	user = User.create(user_params)
+	# 	render json: user
+	# end 
 
 	def update
 		user = find_user
@@ -48,6 +48,6 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:username, :password_digest)
+		params.require(:user).permit(:username, :password)
 	end
 end
